e3dffb23eb79284607258e5ddbe9586c8f2f55e1
Dylandurand <dylan.dd@outlook.com>
2024-06-05 10:03:12 +0200

    feat(controllers/comments_controller.rb, helpers/comments_helper.rb) : Create the possibility to add comment on article

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root 'articles#index'
-  resources :articles # Une ligne pour plusieurs routes Index, Show, Update, Delete
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :articles do # Une ligne pour plusieurs routes Index, Show, Update, Delete
+    resources :comments
+  end
 end
